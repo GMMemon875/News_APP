ee28d4ea268c65a4a5d4bb29ea5bfa1e0527e034
GMMemon875 <ghulammemon875@gmail.com>
2024-07-19 00:58:05 +0500

    Add some coments

--- src/Componanat/FatchData.jsx
+++ src/Componanat/FatchData.jsx
@@ -6,6 +6,7 @@ const FatchData = ({ cat }) => {
   const [data, setData] = useState("");
 
   const fetchData = async () => {
+    // yaha per ham ni data ko fatch keya and us ke categories ke jo be ham ni option call kar rahe hen wo data fatch ho jaie gi
     await axios
 
       .get(
@@ -14,7 +15,7 @@ const FatchData = ({ cat }) => {
           : "https://newsapi.org/v2/top-headlines?country=us&apiKey=19636b15f15b439aa4f6eb0a8ce38c4f"
       )
 
-      .then((res) => setData(res.data.articles));
+      .then((res) => setData(res.data.articles)); // jab data response men aa gaie to ham useState he help se us data men se aiticales wale data ko apni web per show karengi
   };
   useEffect(() => {
     fetchData();
@@ -29,37 +30,42 @@ const FatchData = ({ cat }) => {
         style={{ minHeight: "100vh" }}
       >
         {data
-          ? data.map((items, index) => (
-              <>
-                <div
-                  className="container my-3 p-3"
-                  style={{ width: "600px", boxShadow: "2px 2px 10px silver" }}
-                >
-                  <h5 className="my-4">{items.title}</h5>
-                  <div className="my-4 d-flex justify-content-center align-items-center">
-                    <img
-                      src={items.urlToImage}
-                      alt="Sorry Image is Not found"
-                      className="img-fluid"
-                      style={{
-                        width: "100%",
-                        height: "300px",
-                        objectFit: "cover",
-                      }}
-                    />
-                  </div>
+          ? data.map(
+              (
+                items,
+                index // us data ko map karengi
+              ) => (
+                <>
+                  <div
+                    className="container my-3 p-3"
+                    style={{ width: "600px", boxShadow: "2px 2px 10px silver" }}
+                  >
+                    <h5 className="my-4">{items.title}</h5>
+                    <div className="my-4 d-flex justify-content-center align-items-center">
+                      <img
+                        src={items.urlToImage}
+                        alt="Sorry Image is Not found"
+                        className="img-fluid"
+                        style={{
+                          width: "100%",
+                          height: "300px",
+                          objectFit: "cover",
+                        }}
+                      />
+                    </div>
 
-                  <p>{items.description}</p>
-                  <p>{items.content}</p>
-                  <Link to={items.url} target="blank">
-                    <button type="button" class="btn btn-primary">
-                      {" "}
-                      View More..
-                    </button>
-                  </Link>
-                </div>
-              </>
-            ))
+                    <p>{items.description}</p>
+                    <p>{items.content}</p>
+                    <Link to={items.url} target="blank">
+                      <button type="button" class="btn btn-primary">
+                        {" "}
+                        View More..
+                      </button>
+                    </Link>
+                  </div>
+                </>
+              )
+            )
           : "Loading.... Please Wait"}{" "}
       </div>
     </div>

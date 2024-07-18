249c52f75e7ddb8690fcb66a8d441c397f7b27b7
GMMemon875 <ghulammemon875@gmail.com>
2024-07-18 13:26:56 +0500

    add same comments

--- src/App.js
+++ src/App.js
@@ -1,47 +1,41 @@
 // import logo from './logo.svg';
-import './App.css';
-import { BrowserRouter, Routes ,Route } from 'react-router-dom';
-import Navbar from './Componanat/Navbar';
-import Home from './Componanat/Home';
-import Footer from './Componanat/Footer';
-import FatchData from './Componanat/FatchData';
-import Hero from './Componanat/Hero';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Componanat/Navbar";
+import Home from "./Componanat/Home";
+import Footer from "./Componanat/Footer";
+import FatchData from "./Componanat/FatchData";
+import Hero from "./Componanat/Hero";
 
 function App() {
   return (
-    <> 
-    <BrowserRouter>
-         <Navbar />
-         <Hero />
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Hero />
 
-    <Routes>
-
-      <Route  path='/home' element={<Home />}></Route>
-      <Route  path='/business' element={< FatchData cat="business"/>}></Route>
-      <Route  path='/general' element={< FatchData cat="general"/>}></Route>
-      <Route  path='/sports' element={< FatchData cat="sports"/>}></Route>
-      <Route  path='/health' element={< FatchData cat="health"/>}></Route>
-      <Route  path='/science' element={< FatchData cat="science"/>}></Route>
-      <Route  path='/technology' element={< FatchData cat="technology"/>}></Route>
-      <Route  path='/entertainment' element={< FatchData cat="entertainment"/>}></Route>
-       
-
-
-      
-
-       
-    </Routes> 
-    <Footer />
-    
-    </BrowserRouter>
-    
- 
-
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route
+            path="/business"
+            element={<FatchData cat="business" />}
+          ></Route>
+          <Route path="/general" element={<FatchData cat="general" />}></Route>
+          <Route path="/sports" element={<FatchData cat="sports" />}></Route>
+          <Route path="/health" element={<FatchData cat="health" />}></Route>
+          <Route path="/science" element={<FatchData cat="science" />}></Route>
+          <Route
+            path="/technology"
+            element={<FatchData cat="technology" />}
+          ></Route>
+          <Route
+            path="/entertainment"
+            element={<FatchData cat="entertainment" />}
+          ></Route>
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </>
-
-
-   
- 
   );
 }
 
--- src/Componanat/FatchData.jsx
+++ src/Componanat/FatchData.jsx
@@ -1,58 +1,69 @@
-import React, { useEffect , useState } from 'react'
-import axios from 'axios'
-import { Link } from 'react-router-dom'
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { Link } from "react-router-dom";
 
+const FatchData = ({ cat }) => {
+  const [data, setData] = useState("");
 
-const FatchData = ({cat}) => {
-    const [data, setData] =useState("")
+  const fetchData = async () => {
+    await axios
 
-    const fetchData = async ()=>{
-        await axios
-        
-        .get(
-        cat ? `https://newsapi.org/v2/top-headlines?country=in&category=${cat}&apiKey=19636b15f15b439aa4f6eb0a8ce38c4f`
-         :'https://newsapi.org/v2/top-headlines?country=in&apiKey=19636b15f15b439aa4f6eb0a8ce38c4f')
+      .get(
+        cat
+          ? `https://newsapi.org/v2/top-headlines?country=&category=${cat}&apiKey=19636b15f15b439aa4f6eb0a8ce38c4f`
+          : "https://newsapi.org/v2/top-headlines?country=us&apiKey=19636b15f15b439aa4f6eb0a8ce38c4f"
+      )
 
-        .then((res) =>  setData(res.data.articles
-            ))
-    };
-                useEffect (() =>{ fetchData ();} ,[ cat])
-
-
+      .then((res) => setData(res.data.articles));
+  };
+  useEffect(() => {
+    fetchData();
+  }, [cat]);
 
   return (
-    <div className='container my-4' >
-       <h1> Top Headlinne</h1> 
-      
+    <div className="container my-4">
+      <h1> Top Headlinne</h1>
 
-            <div className= ' cantianer d-flex justify-content-center align-items-center flex-column my-4'  style={{minHeight:'100vh'}}>{data ? data.map((items,index) => ( 
-            <>
+      <div
+        className=" cantianer d-flex justify-content-center align-items-center flex-column my-4"
+        style={{ minHeight: "100vh" }}
+      >
+        {data
+          ? data.map((items, index) => (
+              <>
+                <div
+                  className="container my-3 p-3"
+                  style={{ width: "600px", boxShadow: "2px 2px 10px silver" }}
+                >
+                  <h5 className="my-4">{items.title}</h5>
+                  <div className="my-4 d-flex justify-content-center align-items-center">
+                    <img
+                      src={items.urlToImage}
+                      alt="Sorry Image is Not found"
+                      className="img-fluid"
+                      style={{
+                        width: "100%",
+                        height: "300px",
+                        objectFit: "cover",
+                      }}
+                    />
+                  </div>
 
-             <div className='container my-3 p-3' style={{width:'600px' ,boxShadow:'2px 2px 10px silver'}}>
-                <h5 className='my-4'>{items.title}</h5>
-                <div className='my-4 d-flex justify-content-center align-items-center' >
-
-                <img src={items.urlToImage} alt='Sorry Image is Not found' className='img-fluid' 
-                style={{width:'100%' ,height:'300px' ,objectFit:'cover'}}
-                
-                />
+                  <p>{items.description}</p>
+                  <p>{items.content}</p>
+                  <Link to={items.url} target="blank">
+                    <button type="button" class="btn btn-primary">
+                      {" "}
+                      View More..
+                    </button>
+                  </Link>
                 </div>
-                 
-                <p>{items.description}</p>
-                <p >{items.content}</p>
-                <Link to={items.url} target='blank' ><button type="button" class="btn btn-primary"> View More..</button>
-</Link>
-
-             </div>
-               
-             
-             </>))  : "Loading.... Please Wait"} </div>
-
-             
-
-      
+              </>
+            ))
+          : "Loading.... Please Wait"}{" "}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FatchData
+export default FatchData;
